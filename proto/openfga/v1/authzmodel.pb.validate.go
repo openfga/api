30cb0a8ef8afb2ae6b6855ec028707fda9d3c658
José Padilla <jose.padilla@okta.com>
2023-08-21 16:20:54 -0400

    feat: add `conditions` to AuthorizationModel

--- proto/openfga/v1/authzmodel.pb.validate.go
+++ proto/openfga/v1/authzmodel.pb.validate.go
@@ -104,6 +104,52 @@ func (m *AuthorizationModel) validate(all bool) error {
 
 	}
 
+	{
+		sorted_keys := make([]string, len(m.GetConditions()))
+		i := 0
+		for key := range m.GetConditions() {
+			sorted_keys[i] = key
+			i++
+		}
+		sort.Slice(sorted_keys, func(i, j int) bool { return sorted_keys[i] < sorted_keys[j] })
+		for _, key := range sorted_keys {
+			val := m.GetConditions()[key]
+			_ = val
+
+			// no validation rules for Conditions[key]
+
+			if all {
+				switch v := interface{}(val).(type) {
+				case interface{ ValidateAll() error }:
+					if err := v.ValidateAll(); err != nil {
+						errors = append(errors, AuthorizationModelValidationError{
+							field:  fmt.Sprintf("Conditions[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				case interface{ Validate() error }:
+					if err := v.Validate(); err != nil {
+						errors = append(errors, AuthorizationModelValidationError{
+							field:  fmt.Sprintf("Conditions[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				}
+			} else if v, ok := interface{}(val).(interface{ Validate() error }); ok {
+				if err := v.Validate(); err != nil {
+					return AuthorizationModelValidationError{
+						field:  fmt.Sprintf("Conditions[%v]", key),
+						reason: "embedded message failed validation",
+						cause:  err,
+					}
+				}
+			}
+
+		}
+	}
+
 	if len(errors) > 0 {
 		return AuthorizationModelMultiError(errors)
 	}
