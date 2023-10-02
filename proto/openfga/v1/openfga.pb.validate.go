883c47e4981bd261eae2f583b3fabe07e487c5ff
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2023-10-01 19:14:00 -0700

    chore: contextual tuples use WriteRequestTupleKey

--- proto/openfga/v1/openfga.pb.validate.go
+++ proto/openfga/v1/openfga.pb.validate.go
@@ -776,6 +776,182 @@ var _ interface {
 	ErrorName() string
 } = TupleKeysValidationError{}
 
+// Validate checks the field values on WriteRequestTupleKey with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *WriteRequestTupleKey) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on WriteRequestTupleKey with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// WriteRequestTupleKeyMultiError, or nil if none found.
+func (m *WriteRequestTupleKey) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *WriteRequestTupleKey) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if len(m.GetUser()) > 512 {
+		err := WriteRequestTupleKeyValidationError{
+			field:  "User",
+			reason: "value length must be at most 512 bytes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if m.GetRelation() != "" {
+
+		if !_WriteRequestTupleKey_Relation_Pattern.MatchString(m.GetRelation()) {
+			err := WriteRequestTupleKeyValidationError{
+				field:  "Relation",
+				reason: "value does not match regex pattern \"^[^:#@\\\\s]{1,50}$\"",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+	}
+
+	if m.GetObject() != "" {
+
+		if !_WriteRequestTupleKey_Object_Pattern.MatchString(m.GetObject()) {
+			err := WriteRequestTupleKeyValidationError{
+				field:  "Object",
+				reason: "value does not match regex pattern \"^[^\\\\s]{2,256}$\"",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+	}
+
+	if all {
+		switch v := interface{}(m.GetCondition()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, WriteRequestTupleKeyValidationError{
+					field:  "Condition",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, WriteRequestTupleKeyValidationError{
+					field:  "Condition",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetCondition()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return WriteRequestTupleKeyValidationError{
+				field:  "Condition",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if len(errors) > 0 {
+		return WriteRequestTupleKeyMultiError(errors)
+	}
+
+	return nil
+}
+
+// WriteRequestTupleKeyMultiError is an error wrapping multiple validation
+// errors returned by WriteRequestTupleKey.ValidateAll() if the designated
+// constraints aren't met.
+type WriteRequestTupleKeyMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m WriteRequestTupleKeyMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m WriteRequestTupleKeyMultiError) AllErrors() []error { return m }
+
+// WriteRequestTupleKeyValidationError is the validation error returned by
+// WriteRequestTupleKey.Validate if the designated constraints aren't met.
+type WriteRequestTupleKeyValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e WriteRequestTupleKeyValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e WriteRequestTupleKeyValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e WriteRequestTupleKeyValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e WriteRequestTupleKeyValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e WriteRequestTupleKeyValidationError) ErrorName() string {
+	return "WriteRequestTupleKeyValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e WriteRequestTupleKeyValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sWriteRequestTupleKey.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = WriteRequestTupleKeyValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = WriteRequestTupleKeyValidationError{}
+
+var _WriteRequestTupleKey_Relation_Pattern = regexp.MustCompile("^[^:#@\\s]{1,50}$")
+
+var _WriteRequestTupleKey_Object_Pattern = regexp.MustCompile("^[^\\s]{2,256}$")
+
 // Validate checks the field values on ContextualTupleKeys with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
