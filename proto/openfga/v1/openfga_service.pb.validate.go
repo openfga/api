ed3d38b89835999cf9c0e7e7e1e407f098ca0bf8
José Padilla <jpadilla@webapplicate.com>
2023-12-07 16:46:30 -0500

    fix: revert assertions tuple key order change (#123)

--- proto/openfga/v1/openfga_service.pb.validate.go
+++ proto/openfga/v1/openfga_service.pb.validate.go
@@ -5755,6 +5755,153 @@ var _ interface {
 	ErrorName() string
 } = ListStoresResponseValidationError{}
 
+// Validate checks the field values on AssertionTupleKey with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// first error encountered is returned, or nil if there are no violations.
+func (m *AssertionTupleKey) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on AssertionTupleKey with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// AssertionTupleKeyMultiError, or nil if none found.
+func (m *AssertionTupleKey) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *AssertionTupleKey) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if m.GetObject() != "" {
+
+		if !_AssertionTupleKey_Object_Pattern.MatchString(m.GetObject()) {
+			err := AssertionTupleKeyValidationError{
+				field:  "Object",
+				reason: "value does not match regex pattern \"^[^\\\\s]{2,256}$\"",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+	}
+
+	if m.GetRelation() != "" {
+
+		if !_AssertionTupleKey_Relation_Pattern.MatchString(m.GetRelation()) {
+			err := AssertionTupleKeyValidationError{
+				field:  "Relation",
+				reason: "value does not match regex pattern \"^[^:#@\\\\s]{1,50}$\"",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+	}
+
+	if len(m.GetUser()) > 512 {
+		err := AssertionTupleKeyValidationError{
+			field:  "User",
+			reason: "value length must be at most 512 bytes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if len(errors) > 0 {
+		return AssertionTupleKeyMultiError(errors)
+	}
+
+	return nil
+}
+
+// AssertionTupleKeyMultiError is an error wrapping multiple validation errors
+// returned by AssertionTupleKey.ValidateAll() if the designated constraints
+// aren't met.
+type AssertionTupleKeyMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m AssertionTupleKeyMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m AssertionTupleKeyMultiError) AllErrors() []error { return m }
+
+// AssertionTupleKeyValidationError is the validation error returned by
+// AssertionTupleKey.Validate if the designated constraints aren't met.
+type AssertionTupleKeyValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e AssertionTupleKeyValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e AssertionTupleKeyValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e AssertionTupleKeyValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e AssertionTupleKeyValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e AssertionTupleKeyValidationError) ErrorName() string {
+	return "AssertionTupleKeyValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e AssertionTupleKeyValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sAssertionTupleKey.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = AssertionTupleKeyValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = AssertionTupleKeyValidationError{}
+
+var _AssertionTupleKey_Object_Pattern = regexp.MustCompile("^[^\\s]{2,256}$")
+
+var _AssertionTupleKey_Relation_Pattern = regexp.MustCompile("^[^:#@\\s]{1,50}$")
+
 // Validate checks the field values on Assertion with the rules defined in the
 // proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
