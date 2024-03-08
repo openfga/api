6b1fb8f5bb77099c472c3ecb711f72dc0cb43f51
Will Vedder <willvedd@gmail.com>
2024-03-08 09:57:59 -0500

    Adding listusers response

--- proto/openfga/v1/openfga_service.pb.validate.go
+++ proto/openfga/v1/openfga_service.pb.validate.go
@@ -599,6 +599,180 @@ var _ListUsersRequest_AuthorizationModelId_Pattern = regexp.MustCompile("^[ABCDE
 
 var _ListUsersRequest_Relation_Pattern = regexp.MustCompile("^[^:#@\\s]{1,50}$")
 
+// Validate checks the field values on UserOrUserGroupOrWildcard with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *UserOrUserGroupOrWildcard) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on UserOrUserGroupOrWildcard with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// UserOrUserGroupOrWildcardMultiError, or nil if none found.
+func (m *UserOrUserGroupOrWildcard) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *UserOrUserGroupOrWildcard) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if !_UserOrUserGroupOrWildcard_Type_Pattern.MatchString(m.GetType()) {
+		err := UserOrUserGroupOrWildcardValidationError{
+			field:  "Type",
+			reason: "value does not match regex pattern \"^[^:#@\\\\s]{1,254}$\"",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if !_UserOrUserGroupOrWildcard_Id_Pattern.MatchString(m.GetId()) {
+		err := UserOrUserGroupOrWildcardValidationError{
+			field:  "Id",
+			reason: "value does not match regex pattern \"[^#:\\\\s]+$\"",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if m.GetRelation() != "" {
+
+		if !_UserOrUserGroupOrWildcard_Relation_Pattern.MatchString(m.GetRelation()) {
+			err := UserOrUserGroupOrWildcardValidationError{
+				field:  "Relation",
+				reason: "value does not match regex pattern \"^[^:#@\\\\s]{1,50}$\"",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+	}
+
+	if all {
+		switch v := interface{}(m.GetWildcard()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, UserOrUserGroupOrWildcardValidationError{
+					field:  "Wildcard",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, UserOrUserGroupOrWildcardValidationError{
+					field:  "Wildcard",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetWildcard()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return UserOrUserGroupOrWildcardValidationError{
+				field:  "Wildcard",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if len(errors) > 0 {
+		return UserOrUserGroupOrWildcardMultiError(errors)
+	}
+
+	return nil
+}
+
+// UserOrUserGroupOrWildcardMultiError is an error wrapping multiple validation
+// errors returned by UserOrUserGroupOrWildcard.ValidateAll() if the
+// designated constraints aren't met.
+type UserOrUserGroupOrWildcardMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m UserOrUserGroupOrWildcardMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m UserOrUserGroupOrWildcardMultiError) AllErrors() []error { return m }
+
+// UserOrUserGroupOrWildcardValidationError is the validation error returned by
+// UserOrUserGroupOrWildcard.Validate if the designated constraints aren't met.
+type UserOrUserGroupOrWildcardValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e UserOrUserGroupOrWildcardValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e UserOrUserGroupOrWildcardValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e UserOrUserGroupOrWildcardValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e UserOrUserGroupOrWildcardValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e UserOrUserGroupOrWildcardValidationError) ErrorName() string {
+	return "UserOrUserGroupOrWildcardValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e UserOrUserGroupOrWildcardValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sUserOrUserGroupOrWildcard.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = UserOrUserGroupOrWildcardValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = UserOrUserGroupOrWildcardValidationError{}
+
+var _UserOrUserGroupOrWildcard_Type_Pattern = regexp.MustCompile("^[^:#@\\s]{1,254}$")
+
+var _UserOrUserGroupOrWildcard_Id_Pattern = regexp.MustCompile("[^#:\\s]+$")
+
+var _UserOrUserGroupOrWildcard_Relation_Pattern = regexp.MustCompile("^[^:#@\\s]{1,50}$")
+
 // Validate checks the field values on ListUsersResponse with the rules defined
 // in the proto definition for this message. If any rules are violated, the
 // first error encountered is returned, or nil if there are no violations.
