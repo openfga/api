8805ee83c35fe24155bac8f52c2d9c673444eedb
Will Vedder <willvedd@gmail.com>
2024-04-16 16:12:14 -0400

    Regenerating types

--- proto/openfga/v1/openfga_service.pb.validate.go
+++ proto/openfga/v1/openfga_service.pb.validate.go
@@ -1346,6 +1346,40 @@ func (m *ListUsersResponse) validate(all bool) error {
 
 	}
 
+	for idx, item := range m.GetExceptUsers() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ListUsersResponseValidationError{
+						field:  fmt.Sprintf("ExceptUsers[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ListUsersResponseValidationError{
+						field:  fmt.Sprintf("ExceptUsers[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ListUsersResponseValidationError{
+					field:  fmt.Sprintf("ExceptUsers[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return ListUsersResponseMultiError(errors)
 	}
