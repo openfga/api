db6f987744909bb59e3bd8e1d4ebf47b2f35d36f
Will Vedder <willvedd@gmail.com>
2024-06-12 13:24:07 -0400

    Removing excluded users

--- proto/openfga/v1/openfga_service.pb.validate.go
+++ proto/openfga/v1/openfga_service.pb.validate.go
@@ -722,40 +722,6 @@ func (m *ListUsersResponse) validate(all bool) error {
 
 	}
 
-	for idx, item := range m.GetExcludedUsers() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, ListUsersResponseValidationError{
-						field:  fmt.Sprintf("ExcludedUsers[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, ListUsersResponseValidationError{
-						field:  fmt.Sprintf("ExcludedUsers[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return ListUsersResponseValidationError{
-					field:  fmt.Sprintf("ExcludedUsers[%v]", idx),
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
-		}
-
-	}
-
 	if len(errors) > 0 {
 		return ListUsersResponseMultiError(errors)
 	}
