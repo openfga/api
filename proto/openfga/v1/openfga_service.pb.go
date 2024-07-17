3014a86f9626736d79d30509d3f1372c5ce19acd
Ewan Harris <ewan.harris@okta.com>
2024-07-17 10:26:11 +0200

    doc: correct consistency reference in doc string (#182)
    * doc: correct consistency reference in doc string
    
    * chore: remove dx from codeowners as we're moving off feature branch

--- proto/openfga/v1/openfga_service.pb.go
+++ proto/openfga/v1/openfga_service.pb.go
@@ -35,7 +35,7 @@ type ConsistencyPreference int32
 const (
 	// Default if not set. Behavior will be the same as MINIMIZE_LATENCY
 	ConsistencyPreference_UNSPECIFIED ConsistencyPreference = 0
-	// Minimize latency at the potential expense of higher consistency.
+	// Minimize latency at the potential expense of lower consistency.
 	ConsistencyPreference_MINIMIZE_LATENCY ConsistencyPreference = 100
 	// Prefer higher consistency, at the potential expense of increased latency.
 	ConsistencyPreference_HIGHER_CONSISTENCY ConsistencyPreference = 200
