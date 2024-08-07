c96ec773cae9caf2a9536856bf1fa206a42ef7ef
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2024-08-07 13:13:05 -0700

    feat: add support for contextual tuples in assertions API (#185)
    * Revert "Reverting contextual tuples in assertions API (#157) (#167)"
    
    This reverts commit 5b0b4941f3e0ae78475a6a4940f5a49cc243b257.
    
    * undo go.mod changes
    
    * amend API descriptions
    
    * run make
    
    * apply suggestion

--- proto/openfga/v1/openfga_service.pb.validate.go
+++ proto/openfga/v1/openfga_service.pb.validate.go
@@ -6490,6 +6490,51 @@ func (m *Assertion) validate(all bool) error {
 
 	// no validation rules for Expectation
 
+	if len(m.GetContextualTuples()) > 20 {
+		err := AssertionValidationError{
+			field:  "ContextualTuples",
+			reason: "value must contain no more than 20 item(s)",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	for idx, item := range m.GetContextualTuples() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, AssertionValidationError{
+						field:  fmt.Sprintf("ContextualTuples[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, AssertionValidationError{
+						field:  fmt.Sprintf("ContextualTuples[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return AssertionValidationError{
+					field:  fmt.Sprintf("ContextualTuples[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return AssertionMultiError(errors)
 	}
