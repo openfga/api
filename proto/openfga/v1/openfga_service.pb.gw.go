9046c5c8876cc8e95bd072ba90e9dcdeab28fde8
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2024-09-05 20:26:43 -0700

    build: migrate to buf v2

--- proto/openfga/v1/openfga_service.pb.gw.go
+++ proto/openfga/v1/openfga_service.pb.gw.go
@@ -1083,6 +1083,7 @@ func local_request_OpenFGAService_ListUsers_0(ctx context.Context, marshaler run
 // UnaryRPC     :call OpenFGAServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterOpenFGAServiceHandlerFromEndpoint instead.
+// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterOpenFGAServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server OpenFGAServiceServer) error {
 
 	mux.Handle("POST", pattern_OpenFGAService_Read_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -1523,21 +1524,21 @@ func RegisterOpenFGAServiceHandlerServer(ctx context.Context, mux *runtime.Serve
 // RegisterOpenFGAServiceHandlerFromEndpoint is same as RegisterOpenFGAServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterOpenFGAServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.DialContext(ctx, endpoint, opts...)
+	conn, err := grpc.NewClient(endpoint, opts...)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
@@ -1555,7 +1556,7 @@ func RegisterOpenFGAServiceHandler(ctx context.Context, mux *runtime.ServeMux, c
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "OpenFGAServiceClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "OpenFGAServiceClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "OpenFGAServiceClient" to call the correct interceptors.
+// "OpenFGAServiceClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterOpenFGAServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client OpenFGAServiceClient) error {
 
 	mux.Handle("POST", pattern_OpenFGAService_Read_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
