50d1c0a9bae33830300a0d64c9cbbc7c79679e62
Sujitha A V <sujithaav2012@gmail.com>
2024-10-07 19:35:34 +0530

    Add contextual tuples to Expand API

--- proto/openfga/v1/openfga_service.pb.validate.go
+++ proto/openfga/v1/openfga_service.pb.validate.go
@@ -2801,6 +2801,35 @@ func (m *ExpandRequest) validate(all bool) error {
 		errors = append(errors, err)
 	}
 
+	if all {
+		switch v := interface{}(m.GetContextualTuples()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ExpandRequestValidationError{
+					field:  "ContextualTuples",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ExpandRequestValidationError{
+					field:  "ContextualTuples",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetContextualTuples()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ExpandRequestValidationError{
+				field:  "ContextualTuples",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return ExpandRequestMultiError(errors)
 	}
